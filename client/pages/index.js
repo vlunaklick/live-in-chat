e53d40e5e07bcb4bc9b89de0532b900627bf6b73
Valentín Lunaklick <v.lunaklick@gmail.com>
2022-02-12 14:51:13 -0300

    feat: first commit

---
+++ client/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Live in Chat</title>
+				<meta name='description' content='Chatting app' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+		</div>
+	)
+}
